9f841acaee02e0dc81f07bc45357b32107828ead
Yasi Gorji <yasigorji25@gmail.com>
2020-10-23 13:48:02 +1000

    updated

--- client/src/chart.js
+++ client/src/chart.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Pie } from 'react-chartjs-2';
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import { useParams } from "react-router";
 import { useHistory } from "react-router-dom";
 import Example from "./Analysis";
 import ReactWordcloud from 'react-wordcloud';
-//import MyWordcloud from "./wordCloud.js"
 
 export default function pie() {
 
@@ -20,9 +19,9 @@ function ShowPieChart() {
     const [pieDataTrump, setPieDataTrump] = useState([]);
     const [pieDataBiden, setPieDataBiden] = useState([]);
     const [wordsTP, setWordsTP] = useState([]);
+    const [wordsB, setWordsB] = useState([]);
     const history = useHistory();
-    let { hashtag } = useParams();
-    const navigateTo = () => history.push(`/sentiment/insight`);
+    let { hashtag } = useParams();;
     const dataTrump = {
         labels: [
             'Positive',
@@ -52,6 +51,22 @@ function ShowPieChart() {
 
             labels: {
                 padding: 20
+            }
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem, data) {
+                    var dataset = data.datasets[tooltipItem.datasetIndex];
+                    var total = dataset.data.reduce(function (previousValue, currentValue, currentIndex, array) {
+                        return previousValue + currentValue;
+                    });
+                    var currentValue = dataset.data[tooltipItem.index];
+                    var percentage = Math.floor(((currentValue / total) * 100) + 0.5);
+                    return percentage + "%";
+                },
+                title: function (tooltipItem, data) {
+                    return data.labels[tooltipItem[0].index];
+                }
             }
         }
     }
@@ -85,72 +100,73 @@ function ShowPieChart() {
                 padding: 20
 
             }
+        },
+        tooltips: {
+            enabled: false
+        },
+        plugins: {
+            datalabels: {
+                formatter: (value, ctx) => {
+                    let sum = 0;
+                    let dataArr = ctx.chart.data.datasets[0].data;
+                    dataArr.map(data => {
+                        sum += data;
+                    });
+                    let percentage = (value * 100 / sum).toFixed(2) + "%";
+                    return percentage;
+                },
+                color: 'black',
+            }
         }
     }
     useEffect(() => {
-
-
         fetch(`/sentiment/${hashtag}`)
             .then(res => res.json())
-            .then(data => setPieDataTrump(data.TrumpFeedback) & setPieDataBiden(data.BidenFeedback) & setWordsTP(data.Keywords.TrumpWordCloud)
+            .then(data => setPieDataTrump(data.TrumpFeedback) & setPieDataBiden(data.BidenFeedback) & setWordsTP(data.Keywords.TrumpWordCloud) &
+                setWordsB(data.Keywords.BidenWordCloud)
             );
 
-
-
-    }, [hashtag]);
-    const callbacks = {
-        getWordColor: wordsTP => wordsTP.value >= 2 ? "blue" : "red",
-        onWordClick: console.log,
-        onWordMouseOver: console.log,
-        getWordTooltip: wordsTP => `${wordsTP.text} (${wordsTP.value}) [${wordsTP.value >= 2 ? "good" : "bad"}]`,
+    }, []);
+    const callbacksT = {
+        getWordTooltip: wordsTP => `${wordsTP.text} (${wordsTP.value * 15})`,
+    }
+    const callbacksB = {
+        getWordTooltip: wordsB => `${wordsB.text} (${wordsB.value * 15})`,
     }
     const options = {
         rotations: 2,
-        rotationAngles: [-90, 0],
-        fontSizes: [10, 50],
+        rotationAngles: [-45, 45],
+        fontSizes: [12, 50]
+
     };
+
     const size = [600, 400];
     return (
         <div>
             <h3> Feedback for #{hashtag} </h3>
             <Row>
 
-                <Col xs="4">
+                <Col xs="5">
 
-                    <Pie data={dataTrump} width={120}
+                    <Pie data={dataTrump} width={180}
                         height={50} options={optionT} />
-
-                    {/* <Button color="primary" >Get more insight! </Button> */}
                     <ReactWordcloud
-                        callbacks={callbacks}
+                        callbacks={callbacksT}
                         options={options}
                         size={size}
                         words={wordsTP} />
                 </Col>
-                <Col xs="4"></Col>
-                <Col xs="4">
-                    <Pie data={dataBiden} width={120}
-                        height={50} options={optionB} /></Col>
+                <Col xs="2"></Col>
+                <Col xs="5">
+                    <Pie data={dataBiden} width={180}
+                        height={50} options={optionB} />
+                    <ReactWordcloud
+                        callbacks={callbacksB}
+                        options={options}
+                        size={size}
+                        words={wordsB} />
+                </Col>
             </Row>
         </div >
     );
 }
-
-// function MyWordcloud() {
-//     // const [pieDataTrump, setPieDataTrump] = useState([]);
-//     // const [pieDataBiden, setPieDataBiden] = useState([]);
-
-//     let { hashtag } = useParams();
-//     useEffect(() => {
-//         console.log("Heyyyyy");
-//         fetch(`/sentiment/${hashtag}`)
-
-//             .then(res => res.json())
-//             .then(data => console.log(data)
-//             );
-
-
-
-//     }, [hashtag]);
-//     return null;
-// }
